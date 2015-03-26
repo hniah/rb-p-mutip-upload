2b222f38a5a6c0bebf993eefe09afbb7eaf64bb6
Anthony <anthony@futureworkz.com>
2015-03-26 13:49:42 +0700

    install pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150326021445) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
