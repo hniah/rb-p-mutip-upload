561ad30736e9309a453c0cc5e9d007972cedfb16
Maicon <maicon@futureworkz.com>
2015-03-26 10:40:02 +0700

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,47 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require dropzone
+//= require_tree .
+
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
+
+$(document).ready(function(){
+  // disable auto discover
+  Dropzone.autoDiscover = false;
+ 
+
+  var dropzone = new Dropzone (".dropzone", {
+    maxFilesize: 256, // Set the maximum file size to 256 MB
+    paramName: "image[avatar]", // Rails expects the file upload to be something like model[field_name]
+    addRemoveLinks: false // Don't show remove links on dropzone itself.
+  }); 
+
+  dropzone.on("success", function(file) {
+    this.removeFile(file)
+    $.getScript("/images")
+  })
+});
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+(1..100).each do |i|
+  Post.create!(title: "Lipsum Post #{i}", body: %{
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat purus dapibus fermentum sagittis. Fusce in tempus felis. Phasellus a erat ut lorem lacinia bibendum. Vivamus viverra facilisis neque, in scelerisque urna pharetra vel. Donec a est mauris. Integer eget metus quis eros egestas elementum. Integer bibendum risus hendrerit dapibus tempor. Fusce placerat in orci vitae tincidunt.
+  })
+end
