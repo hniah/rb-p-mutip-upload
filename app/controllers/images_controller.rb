e058b1e60b2c2164e325881ac790a0013e97796c
Anthony <anthony@futureworkz.com>
2015-04-01 14:05:07 +0700

    Complete move images  file tmp to origin

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,20 +1,26 @@
 class ImagesController < ApplicationController
+
   def index
+
   	@images = Image.all
     @image = Image.new
+    @image.perform(1)
+
   end
 
   def create
   	@image = Image.new(image_params)
 
     if @image.save
-      render json: { message: "success", fileID: @image.id }, :status => 200
+      render json: { message: "success", fileID: @image.id }, status: 200
     else
-      render json: { error: @image.errors.full_messages.join(',')}, :status => 400
+      render json: { error: @image.errors.full_messages.join(',')}, status: 400
     end     
   end
   private
   def image_params
     params.require(:image).permit(:avatar)
   end
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,23 @@
 class Product < ActiveRecord::Base
   has_many :images
+  attr_accessor :product_images
+  validates  :product_images, presence: true
+
+  def move_image(id, pid)
+    image = ImageTemp.find(id)
+    unless image.present?
+      return
+    end
+
+    Image.create(picture_file_name: image.picture_file_name, picture_content_type: image.picture_content_type,
+                 picture_file_size: image.picture_file_size, picture_updated_at: image.picture_updated_at, product_id:pid)
+
+    s3 = AWS::S3.new
+    styleImage = %w[original_ small_ thumb_ large_]
+    styleImage.each do |style|
+      paperclip_file_path = "public/images/products/#{pid}/#{id}/#{style+image.picture_file_name}"
+      s3.buckets[Rails.configuration.aws[:bucket]].objects[paperclip_file_path].copy_from("public/tmp/#{id}/#{style+image.picture_file_name}", acl: :public_read)
+    end
+    s3.buckets[Rails.configuration.aws[:bucket]].objects.with_prefix("public/images/tmp/#{id}/").delete_all
+  end
 end
