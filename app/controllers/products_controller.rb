6d385debb6f22a57a3224933ee0a2ca5490e56a9
Maicon <maicon@futureworkz.com>
2015-04-07 14:09:03 +0700

    hidden field

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    @product = Product.find(params[:id])
+  end
 
   def new
     @product = Product.new
