b229409158fdc8a88e3bb62a6aa98fb9878a0cbe
Maicon <maicon@futureworkz.com>
2015-04-07 09:46:23 +0700

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,21 +5,21 @@ class Product < ActiveRecord::Base
   validates  :name, presence: true
   validates  :description, presence: true
 
-  def move_image(id, pid)
-    image = ImageTemp.find(id)
+  def move_image(idt, pid)
+    image = ImageTemp.find(idt)
     unless image.present?
       return
     end
-
-    Image.create(picture_file_name: image.picture_file_name, picture_content_type: image.picture_content_type,
+   
+    image = Image.create(picture_file_name: image.picture_file_name, picture_content_type: image.picture_content_type,
                  picture_file_size: image.picture_file_size, picture_updated_at: image.picture_updated_at, product_id:pid)
 
     s3 = AWS::S3.new
     styleImage = %w[original_ small_ thumb_ large_]
     styleImage.each do |style|
-      paperclip_file_path = "public/images/products/#{pid}/#{id}/#{style+image.picture_file_name}"
-      s3.buckets[Rails.configuration.aws[:bucket]].objects[paperclip_file_path].copy_from("public/tmp/#{id}/#{style+image.picture_file_name}", acl: :public_read)
+      paperclip_file_path = "public/images/products/#{image.id}/#{style+image.picture_file_name}"
+      s3.buckets[Rails.configuration.aws[:bucket]].objects[paperclip_file_path].copy_from("public/images/tmp/#{idt}/#{style+image.picture_file_name}", acl: :public_read)
     end
-    s3.buckets[Rails.configuration.aws[:bucket]].objects.with_prefix("public/images/tmp/#{id}/").delete_all
+    s3.buckets[Rails.configuration.aws[:bucket]].objects.with_prefix("public/images/tmp/#{idt}/").delete_all
   end
 end
